881ee0b79d46c244ee553573b611f47d4328fe8f
polypad <84907095+polypad@users.noreply.github.com>
2021-06-21 17:28:33 +0700

    Update metrics

--- contracts/Metric/LiquidityMetric.sol
+++ contracts/Metric/LiquidityMetric.sol
@@ -14,10 +14,12 @@ contract LiquidityMetric is Ownable {
     uint256 public constant totalAllocation = 60e6 * 1e18;
     uint256 public constant tgeAmount = 0;
     uint256 public remainingAmount = totalAllocation - tgeAmount;
+    uint256 public firstTimeReleaseAmount = (totalAllocation * 30) / 100;
     uint256 public eachReleaseAmount = (totalAllocation * 10) / 100;
-    uint256 public nextTimeRelease = now + 30 days;
+    uint256 public nextTimeRelease = now;
     uint256 public releasePeriod = 30 days;
-    uint256 public lastTimeRelease = now + 300 days;
+    uint256 public lastTimeRelease = now + 210 days;
+    bool public firstTimeReleased;
 
     event ReleaseAllocation(
         address indexed _address,
@@ -27,6 +29,7 @@ contract LiquidityMetric is Ownable {
 
     constructor(address _token) public {
         token = IERC20(_token);
+        firstTimeReleased = false;
     }
 
     function balance() public view returns (uint256) {
@@ -42,6 +45,9 @@ contract LiquidityMetric is Ownable {
         uint256 amount = 0;
         if (block.timestamp >= lastTimeRelease) {
             amount = remainingAmount;
+        } else if (!firstTimeReleased) {
+            firstTimeReleased = true;
+            amount = firstTimeReleaseAmount;
         } else {
             if (eachReleaseAmount <= remainingAmount) {
                 amount = eachReleaseAmount;
